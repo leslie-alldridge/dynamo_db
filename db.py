6681b981a23e8812d0c9d9b93ae6986ec1e3388f
leslie-alldridge <leslie.alldridge@gmail.com>
2019-05-24 21:12:16 +1200

    bp to play around with tmrw

--- db.py
+++ db.py
@@ -1,3 +1,38 @@
 import boto3
 
-print('running')
+dynamodb = boto3.resource('dynamodb')
+
+# Create the DynamoDB table.
+table = dynamodb.create_table(
+    TableName='test_table',
+    KeySchema=[
+        {
+            'AttributeName': 'username',
+            'KeyType': 'HASH'
+        },
+        {
+            'AttributeName': 'last_name',
+            'KeyType': 'RANGE'
+        }
+    ],
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'username',
+            'AttributeType': 'S'
+        },
+        {
+            'AttributeName': 'last_name',
+            'AttributeType': 'S'
+        },
+    ],
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5
+    }
+)
+
+# Wait until the table exists.
+table.meta.client.get_waiter('table_exists').wait(TableName='test_table')
+
+# Print out some data about the table.
+print(table.item_count)
